9561b09b947be32160ca4cc46cb2cd966ddd339e
ktantry <kushaltantry@gmail.com>
2015-03-07 16:39:59 -0800

    Final Fizz Buzz

--- js/fizzBuzz.js
+++ js/fizzBuzz.js
@@ -1,1 +1,13 @@
-console.log('Hello');
+var count = 0;
+while (count < 100) {
+	count++;
+	if (count % 3 == 0) {
+		 document.write('fizz\n');
+	}
+	else if (count % 5 == 0) {
+		document.write('buzz\n');
+	}
+	else {
+	document.write(count + '\n');
+	}	
+}
